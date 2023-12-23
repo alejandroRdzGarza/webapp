c8215117d8f33d97e5bc461300010e629f33ec4f
alejandroRdzGarza <maxhema17@gmail.com>
2023-12-23 17:10:38 -0600

    igreo

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
-from dotenv import load_dotenv
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
